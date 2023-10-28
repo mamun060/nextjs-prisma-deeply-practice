0d32d2bf05f7686f168fbcf34550317d68b79a9d
mamun060 <almamun.raju520@gmail.com>
2023-10-28 11:11:22 +0600

    Prisma nextjs aggreation function list and practice text file add here

--- src/app/api/demo/route.js
+++ src/app/api/demo/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { PrismClient } from "@prisma/client";
+// import { PrismClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 export async function GET(){
     try {
@@ -9,18 +10,40 @@ export async function GET(){
     }
 }
 
+// export async function POST(req, res){
+//     try {
+//         const reqBody = await req.json();
+
+//         const prisma = new PrismClient();
+
+//         let result = await prisma.brand.create({
+//             data: reqBody
+//         })
+
+//         return NextResponse.json({ status: "success", data: result })
+
+//     } catch (error) {
+//         console.log("something wrong!", error);
+//     }
+// }
+
 export async function POST(req, res){
     try {
-        const prisma = new PrismClient();
-        const reqBody = await req.json();
+        let reqBody = await req.json();
+        let prisma = new PrismaClient();
 
-        let result = await prisma.brand.create({
+        let reqData = await prisma.brand.create({
             data: reqBody
         })
 
-        return NextResponse.json({ status: "success", data:reqBody })
+        return NextResponse.json(
+            { status: "success", data: reqData } ,
+            {
+            status: 200
+        }
+        )
 
     } catch (error) {
-        console.log("something wrong!", error);
+        console.log("Something is wrong!", error);
     }
 }
